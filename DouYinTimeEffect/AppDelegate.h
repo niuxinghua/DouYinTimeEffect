964d2ff791f71022055d2d08f6b30390c8b20f8d
Qilong Zang <qilong.zang@itcn000105-mac.local>
2018-01-09 18:15:56 +0800

    Initial Commit

---
+++ DouYinTimeEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouYinTimeEffect
+//
+//  Created by Qilong Zang on 09/01/2018.
+//  Copyright © 2018 Qilong Zang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouYinTimeEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DouYinTimeEffect
+//
+//  Created by Qilong Zang on 09/01/2018.
+//  Copyright © 2018 Qilong Zang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
